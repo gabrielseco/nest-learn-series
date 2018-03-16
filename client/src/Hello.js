b7560f65b7c8adb96b6c6e7b057f172f6e2d9b25
Gabriel <ggarciaseco@gmail.com>
2018-03-16 07:57:00 +0100

    build: Single Commas for prettier

--- client/src/Hello.js
+++ client/src/Hello.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { hot } from "react-hot-loader";
-import styles from "./Hello.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { hot } from 'react-hot-loader';
+import styles from './Hello.scss';
 
 const Hello = ({ name }) => (
   <h1 className={styles.title}>
